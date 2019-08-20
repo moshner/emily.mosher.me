09e5a8aeed255d36989e8752db261e0e3a77f6d9
Nick Mosher <nick@mosher.me>
2019-08-19 20:50:34 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'head.html'; ?>
+<?php include 'top.html'; ?>
+<div id="about">
+	<p>I am Emily Mosher (formerly Emily Laut) and I am a grant writer and freelance science journalist. </p>
+	<p>You can find my published work in <a href="http://news.sciencemag.org/sciencenow/2010/01/06-03.html">ScienceNOW</a>, <a href="http://news.discovery.com/contributors/emily-laut/">Discover Blogs</a>, the online portion of <a href="http://blogs.sciencemag.org/newsblog/2009/10/time-to-get-wiki.html">Science</a>, and the <a href="http://www.jhu.edu/jhumag/0409web/wholly.html#pterosaurs">Johns Hopkins Magazine</a>.</p>
+	<p>I spend all the time I can with my husband, <a href="http://nick.mosher.me">Nick</a>, our daughter, and our two cats, Sagan and Hawking.</p> 
+</div>
+<?php include 'footer.html'; ?>
